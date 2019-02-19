c64c197f44256c27191ceea8a134a136dfce2313
Tianye-Zheng <161220175@smail.nju.edu.cn>
2019-02-19 20:47:02 +0800

    my commit

---
+++ MyPicEncoding.py
@@ -0,0 +1,14 @@
+#!usr/bin/python
+# this script is use for generating base64 code of a picture and embed in a post
+
+import base64
+import sys
+
+if len(sys.argv) < 2:
+    print("\nPlease input at least one picture path.\n")
+else:
+    for i in range(1, len(sys.argv)):
+        f = open(sys.argv[i],'rb')
+        ls_f = base64.b64encode(f.read())
+        f.close()
+        print(sys.argv[i] + ",the base64 code of it is: "+ ls_f + '\n')
